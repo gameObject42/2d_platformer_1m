4de820fcb3ba93915723263c3e65ad3243f0fe72
Hiki <48960647+gameObject42@users.noreply.github.com>
2020-09-21 18:43:22 +0530

    Player Movement
    Implemented player movement using characterController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private CharacterController _controller;
+    [SerializeField]
+    private float speed = 20;
+    private float mf;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        Vector2 moveF = new Vector2(mf, 0);
+        _controller.Move(moveF * Time.deltaTime * speed);
+    }
+    public void moveF()
+    {
+        mf = 1; 
+    }
+    public void moveB()
+    {
+        mf = -1;
+    }
+    public void stop()
+    {
+        mf = 0;
     }
 }
