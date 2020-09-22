671226c011c8fd29b87a545aa32a728863e89ba7
Hiki <48960647+gameObject42@users.noreply.github.com>
2020-09-22 11:43:36 +0530

    Custom gravity and collision detection

---
+++ Assets/Scripts/PhysicsObjects.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObjects : MonoBehaviour
+{
+    public float gravityModifire = 1f;
+    public float minGroundNormalY = .65f;
+
+    protected bool isGrounded;
+    protected Vector2 groundNormal;
+    protected Vector2 velocity;
+    protected Rigidbody2D rb2d;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+
+    protected const float minDistanceCollision = 0.001f;
+    protected const float shellRadius = 0.001f;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+    }
+
+    private void FixedUpdate()
+    {
+        velocity += gravityModifire * Physics2D.gravity * Time.deltaTime;
+        isGrounded = false;
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 move = Vector2.up * deltaPosition;
+        Movement(move, true);
+        Debug.Log(hitBuffer);
+    }
+
+    public void Movement(Vector2 move, bool verticalMovement)
+    {
+        float distance = move.magnitude;
+        if(distance > minDistanceCollision)
+        {
+            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+            for(int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+            for(int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if (currentNormal.y > minGroundNormalY)
+                {
+                    isGrounded = true;
+                    if (verticalMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if(projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+
+                }
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+
+            }
+        }
+        rb2d.position = rb2d.position + move.normalized*distance;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float speed = 20;
     private float mf;
+    private Vector2 Y;
+    private bool Grounded;
+    private float jumpHeight = 2;
+    private float gravity = -9.6f;
 
     private void Start()
     {
@@ -15,9 +19,25 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
+        Grounded = _controller.isGrounded;
+        if(Grounded && Y.y < 0)
+        {
+            Y.y = 0f;
+        }
+
         Vector2 moveF = new Vector2(mf, 0);
         _controller.Move(moveF * Time.deltaTime * speed);
+
+        if (Input.GetButtonDown("Jump") && Grounded)
+        {
+            Y.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+        Y.y += gravity * Time.deltaTime;
+        _controller.Move(Y * Time.deltaTime);
     }
+
+
+
     public void moveF()
     {
         mf = 1; 
