00010faa6eaccc2444fb08b4c3d51ee736a7301c
Hiki <48960647+gameObject42@users.noreply.github.com>
2020-09-28 18:15:39 +0530

    some overall improvements

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; set; }
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Warning: Multiple " + this +" in Scene");
+            Destroy(this);
+        }
+    }
+
+    public enum GameState
+    {
+        MainMenu,
+        Playing,
+        PauseMenu,
+        PlayerDead,
+        GameOver,
+        ShopMenu
+    }
+    public GameState _gameState;
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum PlayerState
+    {
+        Ideal,
+        Running,
+        Attack
+    }
+    public PlayerState _playerState;
+
     public float speed;
     public float jumpforce;
     private float moveInput;
@@ -14,26 +22,40 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatisGround;
+    private SpriteRenderer p;
 
     private Rigidbody2D rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        _playerState = PlayerState.Ideal;
+        p = GetComponent<SpriteRenderer>();
     }
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatisGround);
 
         //moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if(_playerState == PlayerState.Running)
+        {
+            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+            Debug.Log(rb.velocity);
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveInput * 0, rb.velocity.y);
+        }
+        //rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
         if(facingRigt == false && moveInput > 0)
         {
+            //p.flipX = false;
             Flip();
         }
         else if(facingRigt == true && moveInput < 0)
         {
+            //p.flipX = true;
             Flip();
         }
     }
@@ -66,14 +88,17 @@ public class PlayerController : MonoBehaviour
     }
     public void MF()
     {
+        _playerState = PlayerState.Running;
         moveInput = 1;
     }
     public void MB()
     {
+        _playerState = PlayerState.Running;
         moveInput = -1;
     }
     public void Stop()
     {
         moveInput = 0;
+        _playerState = PlayerState.Ideal;
     }
 }
